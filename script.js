a3e0c6e62d21c8044fd9403b29517bbcae235d11
adindasitirahmaayukirana-boop <adindasitirahmaayukirana@gmail.com>
2025-10-23 17:19:30 +0700

    update

---
+++ script.js
@@ -0,0 +1,23 @@
+function hasilkan() {
+  var a = parseInt(document.getElementById("number1").value);
+  var b = parseInt(document.getElementById("number2").value);
+  var select = parseInt(document.getElementById("select").value);
+  var hasil = document.getElementById("hasil");
+
+  switch (select) {
+    case 1:
+      return (hasil.innerHTML = a + b);
+      break;
+    case 2:
+      return (hasil.innerHTML = a - b);
+      break;
+
+    case 3:
+      return (hasil.innerHTML = a * b);
+      break;
+
+    case 4:
+      return hasil.innerHTML + a / b;
+      break;
+  }
+}
